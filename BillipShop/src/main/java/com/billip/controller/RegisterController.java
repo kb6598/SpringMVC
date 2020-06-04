fcc9553f699eae38c13dba6978e32308c128a2d8
kb659 <kb659@DESKTOP-RU9MNOF>
2020-06-05 03:44:41 +0900

    update

---
+++ BillipShop/src/main/java/com/billip/controller/RegisterController.java
@@ -0,0 +1,20 @@
+package com.billip.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class RegisterController {
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		return "register";
+	}
+
+
+}
